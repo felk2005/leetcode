0b6df233babe3b27eb7ff18aa98ca39ff243a886
Frank Yuan <xyuan@kabaminc.com>
2015-01-15 14:42:51 +0800

    Initial Commit

---
+++ leetcode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  leetcode
+//
+//  Created by YuanXuan on 1/15/15.
+//  Copyright (c) 2015 felk. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
